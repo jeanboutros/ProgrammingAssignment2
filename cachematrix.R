d0ec080a9c49f5cb05c1ddb3136a8e568b7d1b98
Jean Boutros <jean.boutros@gmail.com>
2018-04-12 19:58:07 +0300

    Coursera Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  cacheInvMatrix <- NULL
+    
+  set <- function(newMatrix) {
+    x <<- newMatrix
+    cacheInvMatrix <<- NULL  
+  }
+  
+  get <- function() cacheMatrix
+  
+  getInvMatrix <- function() cacheInvMatrix
+  
+  setInvMatrix <- function(invMatrix) cacheInvMatrix <<- invMatrix
+  
+  list(set = set, get = get, getInvMatrix = getInvMatrix, setInvMatrix = setInvMatrix)
 }
 
 
@@ -12,4 +26,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invMatrix <- x$getInvMatrix()
+  if(!is.null(invMatrix)) {
+    print("Cached inverse matrix")
+    return(invMatrix)
+  }
+  
+  cachedMatrix <- x$get()
+  invMatrix <- solve(cachedMatrix)
+  x$setInvMatrix(invMatrix)
+  invMatrix
 }
+mm <- matrix(rnorm(9,0,1), nrow = 3, ncol = 3)
+mm
+mmm <- makeCacheMatrix(mm)
+
